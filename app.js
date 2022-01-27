f3ee2950e2ca77b19858a6a3cebbb03fc99a11f4
Thoithoi Shougrakpam <45638892+Toi-Sh@users.noreply.github.com>
2022-01-27 10:02:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+//Bootstrap library for JS
+const correctAnswers= ["B","B","B","B","B"];
+const form= document.querySelector(".quiz-form");
+const result= document.querySelector(".result");
+
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+
+    let score=0
+    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
+
+    //check answers
+    userAnswers.forEach((answer, index)=>{
+        if(answer==correctAnswers[index]){
+            score+=20;
+        }
+    });
+
+    //show result on page
+    scrollTo(0,0);
+    
+    result.classList.remove("d-none");
+    //window object
+    let i=0;
+
+    const timer= setInterval(()=>{
+    result.querySelector("span").textContent= `${i}%`;
+    
+    if(i==score){
+        clearInterval(timer);
+    }else{
+        i++;
+    }
+
+},10);
+
+
+});
+
